f111874d9db26e55b80e69a35f5fdc22232f7ceb
Soman Yadav <52402870+somanyadav@users.noreply.github.com>
2021-03-29 13:27:47 +0530

    Add files via upload

---
+++ Apps/Library/app/src/main/java/com/kushagra/application/library_nu/MainActivity.java
@@ -0,0 +1,37 @@
+package com.kushagra.application.library_nu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.kushagra.application.library_nu.R;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.loadUrl("https://kushagraup.github.io/library_nu_connect/index.html");
+    }
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
---
+++ Apps/Library/app/src/test/java/com/kushagra/application/library_nu/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.kushagra.application.library_nuetu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * kushagra local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
