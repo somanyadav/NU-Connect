c7b502916a4208b097e1a4cfd90e9bf41c695b97
Soman Yadav <52402870+somanyadav@users.noreply.github.com>
2021-03-29 13:30:33 +0530

    Add files via upload

---
+++ Apps/Library/app/build/generated/source/buildConfig/release/com/kushagra/application/library_nu/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kushagra.application.library_nu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.kushagra.application.library_nu";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
